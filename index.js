9378d420cbe06767f4a454deda3cfbedd6cbb5cc
William Killett <williamkillettiii@yahoo.com>
2022-09-20 18:58:01 -0700

    commit tree e971506f287f13e45d08161555b0a40db469d91e

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+   someValue = someValue - 42;
+   if(someValue < 0){
+    someValue = someValue * -1;
+   }
+   return someValue
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    someValue =   (someValue - 42) * 264;
+    if(someValue < 0){
+     someValue = someValue * -1;
+    }
+    return someValue
+   }
+     
+  function distanceTravelledInFeet(startingPoint,endingPoint){
+    endingPoint = (startingPoint - endingPoint) * 264
+    if(endingPoint < 0){
+     endingPoint = endingPoint * -1;
+    }
+    return endingPoint
+      }
+      
+      function calculatesFarePrice(start, destination){
+        destination = (start - destination) * 264
+if(destination < 0){
+ destination = destination * -1;
+}
+if (destination < 400){
+    destination = 0
+}
+else if(destination >= 400 && destination <= 2000){
+    destination = (destination - 400) * 0.02
+}
+else if(destination > 2000 && destination <= 2500){
+    destination = 25
+}
+else if (destination > 2500 ){
+    destination = 'cannot travel that far'
+}
+return destination
+  }
+      
+
+  
+
+  
